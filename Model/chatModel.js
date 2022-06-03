58b870297d447ee8d4b74fecf566b6a7ff939a0f
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-04 01:44:24 +0600

    mongoose added

--- Model/chatModel.js
+++ Model/chatModel.js
@@ -1,10 +1,26 @@
-const mongoose = require('mongoose');
-const chatModel = mongoose.Schema({
-    chatName: {type: String, trim: true},
-    isGroupChat:{type: Boolean, default: false},
-    users:[
-        {type: mongoose.Schema.Types.ObjectId,
-         ref: 'User',
-        }
+const mongoose = require("mongoose");
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
     ],
-})
+    latestMessages: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Chat = mongoose.model("Chat", chatModel);
+module.exports = Chat;
