c315db3782065b849e8c197fc88fbbe3e83aac71
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-17 04:03:19 +0600

    chat api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const chats = require("./data/data");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,17 @@ app.get("/", (req, res) => {
   res.send("Hello World vvvvvvvvvvvvvvvvvvvvvvv!");
 });
 
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+
+
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((chat) => chat._id === req.params.id);
+  res.send(singleChat);
+}
+);
 
 app.listen(port, () => {
   console.log(`Chat app listening on port ${port}!`);
