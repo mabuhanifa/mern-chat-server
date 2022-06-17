0ea4eecbd4a54e208718f3546cffc802bcfa6ecd
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-17 23:28:16 +0600

    jwt secret error solved

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,42 @@
 const asyncHandler = require("express-async-handler");
-const User = require("../Model/userModel");
 const generateToken = require("../config/generateToken");
+const User = require("../Model/userModel");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
+
   if (!name || !email || !password) {
     res.status(400);
-    throw new Error("Please enter all fields");
+    throw new Error("Please Enter all the Fields");
   }
-  const userExist = await User.findOne({ email });
 
-  if (userExist) {
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
     res.status(400);
     throw new Error("User already exists");
   }
+
   const user = await User.create({
     name,
     email,
     password,
     pic,
   });
-  res.send(user);
 
   if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      isAdmin: user.isAdmin,
       pic: user.pic,
-      token: generateToken(user),
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
-    throw new Error("Failed to create the user");
+    throw new Error("User not found");
   }
 });
 
-module.exports = {registerUser};
+module.exports = { registerUser };
