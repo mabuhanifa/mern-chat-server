b784abf84655a0a827bf7a119f8e6049c38363e1
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-18 04:16:18 +0600

    fetch chats added

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const { accessChat } = require("../controllers/chatController");
+const { protect } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-router.route("/").get(protect, accessChat);
-router.route("/group").post(protect, createGroupChat);
-router.route("/rename").put(protect, renameGroup);
-router.route("/groupremove").put(protect, removeFromGroup);
-router.route("/groupadd").put(protect, addToGroup);
+// router.route("/").get(protect, accessChat);
+// router.route("/group").post(protect, createGroupChat);
+// router.route("/rename").put(protect, renameGroup);
+// router.route("/groupremove").put(protect, removeFromGroup);
+// router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
