ab022c9a67cdb650ec56ec43d5049d280f84fe4d
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-17 18:17:45 +0600

    jwt added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,32 @@
-const registerUser 
+const User = require("../Model/userModel");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, pic } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please enter all fields");
+  }
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({ name, email, password, pic });
+  res.send(user);
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token : generateToken(user),
+    });
+  } else {
+    res.status(400)
+    throw new Error("Failed to create the user");
+  }
+});
+
+module.exports = registerUser;
