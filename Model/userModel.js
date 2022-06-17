68f8605229f3c9cd65e431f06a2250506cc4d8f0
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-17 23:36:02 +0600

    bcrypt added

--- Model/userModel.js
+++ Model/userModel.js
@@ -14,5 +14,16 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre('save', async function(next) {
+  if (!this.Modified) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+}
+);
+
 const User = mongoose.model("User", userSchema);
 module.exports = User;
