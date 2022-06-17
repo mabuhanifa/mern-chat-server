8370b60813655a0f4aff5e121568ca2fcb8553b4
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-17 17:12:42 +0600

    mongoDB

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: true,
+    });
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
