90308058aebf4928e3f7e91413e3fd857fb170b5
shourovahmd <moh.abuhanifa@gmail.com>
2022-06-17 23:50:38 +0600

    bcrypt added succesfully to mongodb

--- Model/userModel.js
+++ Model/userModel.js
@@ -15,9 +15,12 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
 
 userSchema.pre('save', async function(next) {
-  if (!this.Modified) {
+  if (!this.isModified) {
     next();
   }
   const salt = await bcrypt.genSalt(10);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const { registerUser } = require("../controllers/userController");
+const { registerUser, authUser } = require("../controllers/userController");
 const router = express.Router();
 
 router.route("/").post(registerUser);
-// router.post9("/login", authUser);
+ router.post("/login", authUser);
 
 module.exports = router;
